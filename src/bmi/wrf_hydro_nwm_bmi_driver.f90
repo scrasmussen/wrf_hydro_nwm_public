e405374800dfa7243d1b9d4879e39b45ae7ff5dd
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-04-05 15:51:20 -0600

    BMI: builds bmi WRF-Hydro object in driver program. Most functions implemented currently as stubs

--- src/bmi/wrf_hydro_nwm_bmi_driver.f90
+++ src/bmi/wrf_hydro_nwm_bmi_driver.f90
@@ -1,10 +1,12 @@
 program wrf_hydro_nwm_bmi_driver
   use bmi_wrf_hydro_nwm_mod, only: bmi_wrf_hydro_nwm
-  ! use bmi_wrf_hydro_nwm_smod!, only: bmi_wrf_hydro_nwm
   implicit none
   class(bmi_wrf_hydro_nwm), allocatable :: wrf_hydro
+  integer :: res
+  print *, "--- Begin running test driver ---"
 
-  ! allocate(wrf_hydro) ! more procedures need to be defined first
+  allocate(wrf_hydro)
+  res = wrf_hydro%initialize("no config file right now")
 
   print *, "--- Finished running test driver ---"
 end program wrf_hydro_nwm_bmi_driver
