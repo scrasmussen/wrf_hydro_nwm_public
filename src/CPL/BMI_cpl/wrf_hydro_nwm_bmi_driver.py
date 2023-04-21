41e2eb404838dbe21251b09798bf2035d8ec4f12
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-04-20 23:07:11 -0600

    bmi: adding python driver and library for WRF-Hydro interoperability

---
+++ src/CPL/BMI_cpl/wrf_hydro_nwm_bmi_driver.py
@@ -0,0 +1,26 @@
+import bmi_wrf_hydro_nwm
+import ctypes as ct
+
+# ----------------------------------------------------------------------
+#  define variables for later use
+current_time = ct.c_double()
+end_time = ct.c_double()
+# ----------------------------------------------------------------------
+
+print('--- Start in Python---')
+wrf_hydro = bmi_wrf_hydro_nwm.symbols_init()
+
+wrf_hydro.initialize()
+wrf_hydro.get_start_time(ct.byref(current_time))
+wrf_hydro.get_end_time(ct.byref(end_time))
+print("start time is", current_time.value, "end time is", end_time.value)
+
+while (current_time.value < end_time.value):
+    wrf_hydro.update()
+    wrf_hydro.get_current_time(ct.byref(current_time))
+    if current_time.value % 10 == 0:
+        current_time.value = 10000
+        print("time =", current_time.value)
+
+wrf_hydro.finalize()
+print('--- Finished running Python test driver ---')
