66e84e97bba05200a87c562664000047d4671d3c
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-05-04 17:04:03 -0600

    bmi: save point, adding function definitions

--- src/CPL/BMI_cpl/bmi_wrf_hydro_nwm.f90
+++ src/CPL/BMI_cpl/bmi_wrf_hydro_nwm.f90
@@ -9,10 +9,12 @@ submodule (bmi_wrf_hydro_nwm_mod) bmi_wrf_hydro_nwm_smod
 
 contains
 
+  ! --- BMI Function Implementation ---
   ! Get the name of the model.
   module procedure wrf_hydro_component_name
     use module_version, only : get_model_version
-    character(len=1024), target :: model_name
+    ! TODO: this save seemed to be needed, check if this is true
+    character(len=BMI_MAX_COMPONENT_NAME), target, save :: model_name
     model_name = get_model_version()
     name => model_name
     bmi_status = BMI_SUCCESS
@@ -21,9 +23,11 @@ contains
   ! Perform startup tasks for the model.
   module procedure wrf_hydro_initialize
     use orchestrator_base, only : orchestrator
-    use module_noahmp_hrldas_driver, only: land_driver_ini
+    use module_noahmp_hrldas_driver, only : land_driver_ini
+    use module_NWM_io_dict, only : initLdasDict
     integer :: end_time
     double precision :: start_time
+    integer :: procID, diagFlag
     bmi_status = BMI_SUCCESS
     call orchestrator%init()
     this%orchestrator = orchestrator
@@ -34,6 +38,10 @@ contains
     this%model%itimestep = int(start_time)
     this%model%ntime = end_time
     this%model%timestep = 1
+
+    procID = 1
+    ! TODO: this is done twice, inefficient, save ldasOutDict first time
+    call initLdasDict(ldasOutDict,procID,diagFlag)
   end procedure ! wrf_hydro_initialize
 
   ! Advance the model one time step.
@@ -65,6 +73,43 @@ contains
     end do
   end procedure ! wrf_hydro_update_until
 
+  ! Count a model's input variables.
+  module procedure wrf_hydro_input_item_count
+    count = input_item_count
+    bmi_status = BMI_SUCCESS
+  end procedure ! wrf_hydro_input_item_count
+
+  ! Count a model's output variables.
+  module procedure wrf_hydro_output_item_count
+    count = output_item_count
+    bmi_status = BMI_SUCCESS
+  end procedure ! wrf_hydro_output_item_count
+
+  ! Get memory use per array element, in bytes.
+  module procedure wrf_hydro_var_itemsize
+    integer :: res
+    integer :: integer_var
+    real :: real_var
+    double precision :: double_var
+    logical :: logical_var
+    character(:), allocatable :: var_type
+    bmi_status = BMI_SUCCESS
+    res = this%get_var_type(name, var_type)
+    select case(var_type)
+    case("integer")
+       size = sizeof(integer_var)
+    case("real")
+       size = sizeof(real_var)
+    case("double precision")
+       size = sizeof(double_var)
+    case("logical")
+       size = sizeof(logical_var)
+    case default
+       size = 0
+       bmi_status = BMI_FAILURE
+    end select
+  end procedure ! wrf_hydro_var_itemsize
+
   !---------------------------------------------------------------------
   ! Should update how this is handeled in main_hrldas_driver
   !---------------------------------------------------------------------
@@ -86,66 +131,92 @@ contains
     bmi_status = BMI_SUCCESS
   end procedure ! wrf_hydro_time_step
 
+  ! Time units of the model.
+  module procedure wrf_hydro_time_units
+    units = "h"
+    bmi_status = BMI_SUCCESS
+  end procedure ! wrf_hydro_time_units
+
   ! Current time of the model.
   module procedure wrf_hydro_current_time
     time = dble(this%model%itimestep)
     bmi_status = BMI_SUCCESS
   end procedure ! wrf_hydro_current_time
 
+  !---------------------------------------------------------------------
+  ! Implemented but should be improved
+  !---------------------------------------------------------------------
+  ! TODO: the following implementations are first takes for ldas, how do we add
+  !       the other parts of the model?
+  ! List a model's output variables.
+  module procedure wrf_hydro_output_var_names
+    allocate(names(output_item_count))
+    names = ldasOutDict%varNames
+    bmi_status = BMI_SUCCESS
+  end procedure ! wrf_hydro_output_var_names
+
+  ! Get the data type of the given variable as a string.
+  module procedure wrf_hydro_var_type
+    integer :: grid, res
+    character(:), allocatable :: var_type
+    res = this%get_var_grid(name, grid)
+    var_type = get_unit_str(ldasOutDict%var_type(grid))
+    type = var_type
+    bmi_status = BMI_SUCCESS
+  end procedure ! wrf_hydro_var_type
+
+  module procedure wrf_hydro_var_units
+    integer :: grid, res
+    res = this%get_var_grid(name, grid)
+    units = ldasOutDict%units(grid)
+    bmi_status = BMI_SUCCESS
+  end procedure ! wrf_hydro_var_units
+
+  ! Get the grid identifier for the given variable.
+  module procedure wrf_hydro_var_grid
+    bmi_status = BMI_SUCCESS
+    select case(name)
+    case("Fortran indexing starts at 1")
+       grid = 0
+    case("IVGTYP")
+       grid = 1
+    case("ISLTYP")
+       grid = 2
+    case default
+       grid = -1
+       bmi_status = BMI_FAILURE
+       print *, "WARNING: variable ", trim(name), " not found"
+    end select
+  end procedure ! wrf_hydro_var_grid
 
   !---------------------------------------------------------------------
   ! STUBS: Section consists of stubs to allow building and testing.
   !        Move above when implemented.
   !---------------------------------------------------------------------
 
-  ! Count a model's input variables.
-  module procedure wrf_hydro_input_item_count
-    count = STUB_I
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_input_item_count
-
-  ! Count a model's output variables.
-  module procedure wrf_hydro_output_item_count
-    count = STUB_I
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_output_item_count
-
   ! List a model's input variables.
   module procedure wrf_hydro_input_var_names
-    bmi_status = BMI_FAILURE
+    allocate(names(2))
+    names(1) = output_item_list(1)
+    names(2) = output_item_list(2)
+    ! names => input_item_list
+    bmi_status = BMI_SUCCESS
   end procedure ! wrf_hydro_input_var_names
-
-  ! List a model's output variables.
-  module procedure wrf_hydro_output_var_names
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_output_var_names
-
-  ! Get the grid identifier for the given variable.
-  module procedure wrf_hydro_var_grid
-    grid = STUB_I
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_var_grid
-
-  ! Get the data type of the given variable as a string.
-  module procedure wrf_hydro_var_type
-    type = STUB_C
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_var_type
-
-  ! Get the units of the given variable.
-  module procedure wrf_hydro_var_units
-    units = STUB_C
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_var_units
-
-  ! Get memory use per array element, in bytes.
-  module procedure wrf_hydro_var_itemsize
-    size = STUB_I
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_var_itemsize
 
   ! Get size of the given variable, in bytes.
   module procedure wrf_hydro_var_nbytes
+    integer :: res, var_size, var_rank, i
+    res =  this%wrf_hydro_var_itemsize(name, size)
+    ! get_variable of var_name
+    var_rank = 0
+    ! var_rank = rank(var_name)
+    ! nbytes = size
+    ! if (var_rank > 0) then
+    !    do i = 1,var_rank
+    !       nbytes = nbytes * size(var_name, i)
+    !    end do
+    ! end if
+
     nbytes = STUB_I
     bmi_status = BMI_FAILURE
   end procedure ! wrf_hydro_var_nbytes
@@ -155,12 +226,6 @@ contains
     location = STUB_C
     bmi_status = BMI_FAILURE
   end procedure ! wrf_hydro_var_location
-
-  ! Time units of the model.
-  module procedure wrf_hydro_time_units
-    units = STUB_C
-    bmi_status = BMI_FAILURE
-  end procedure ! wrf_hydro_time_units
 
   ! Get a copy of values (flattened!) of the given integer variable.
   module procedure wrf_hydro_get_int
@@ -348,4 +413,19 @@ contains
        bmi_status = BMI_FAILURE
     end if
   end procedure
+
+  function get_unit_str(key) result(val)
+    integer, intent(in) ::  key
+    character(BMI_MAX_TYPE_NAME) :: val
+    if (key == 1) then
+       val = "integer"
+    else if (key == 2) then
+       val = "real"
+    else if (key == 3) then
+       val = "double precision"
+    else if (key == 4) then
+       val = "logical"
+    end if
+  end function get_unit_str
+
 end submodule bmi_wrf_hydro_nwm_smod
