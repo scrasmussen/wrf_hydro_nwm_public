588c1494b1dbe50649fe11d6b233988d22b632de
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-05-10 15:30:37 -0600

    bmi: better organization of Python BMI implementation. More formal Python style and BMI functions now follow BMI's rules for Python's return values

--- src/CPL/BMI_cpl/wrf_hydro_nwm_bmi_driver.py
+++ src/CPL/BMI_cpl/wrf_hydro_nwm_bmi_driver.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-import bmi_wrf_hydro_nwm
+import bmi_wrf_hydro_nwm as wrf_hydro
+from bmi_wrf_hydro_nwm import bmi
 import ctypes as ct
 
 
 def main():
-    # ----------------------------------------------------------------------
-    #  define variables for later use
-    current_time = ct.c_double()
-    end_time = ct.c_double()
-    # ----------------------------------------------------------------------
+    print('--- Running in Python---')
 
-    print('--- Start in Python---')
-    wrf_hydro = bmi_wrf_hydro_nwm.symbols_init()
+    name = wrf_hydro.get_component_name()
+    print("Starting", name)
 
     wrf_hydro.initialize()
-    wrf_hydro.get_start_time(ct.byref(current_time))
-    wrf_hydro.get_end_time(ct.byref(end_time))
-    print("start time is", current_time.value, "end time is", end_time.value)
 
-    while (current_time.value < end_time.value):
+    current_time = wrf_hydro.get_start_time()
+    end_time = wrf_hydro.get_end_time()
+    print("start time is", current_time, "end time is", end_time)
+
+    while (current_time < end_time):
         wrf_hydro.update()
-        wrf_hydro.get_current_time(ct.byref(current_time))
-        if current_time.value % 10 == 0:
-            print("time =", current_time.value)
+        current_time = wrf_hydro.get_current_time()
+        if current_time%10 == 0:
+            print("time =", current_time)
 
     wrf_hydro.finalize()
     print('--- Finished running Python test driver ---')
