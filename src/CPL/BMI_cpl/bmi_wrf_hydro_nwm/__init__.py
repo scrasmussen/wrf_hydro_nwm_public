ea9138ad098502e1e5bade8e303561a20b2faf9a
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-05-14 21:30:15 -0600

    bmi: added a number of bind c and Python functions

--- src/CPL/BMI_cpl/bmi_wrf_hydro_nwm/__init__.py
+++ src/CPL/BMI_cpl/bmi_wrf_hydro_nwm/__init__.py
@@ -74,9 +74,90 @@ def get_current_time():
     wrf_h.get_current_time(ct.byref(current_time))
     return current_time.value
 
+# Get the grid identifier for the given variable.
+wrf_h.get_var_grid.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_int)]
+wrf_h.get_var_grid.restype = ct.c_int
+def get_var_grid(name):
+    var_name = (ct.c_char * bmi.BMI_MAX_COMPONENT_NAME)()
+    var_name.value = name.encode()
+    grid_num = ct.c_int()
+    wrf_h.get_var_grid(ct.byref(var_name), ct.byref(grid_num))
+    return grid_num.value
+
+# Get the total number of elements in the computational grid.
+wrf_h.get_grid_size.argtypes = \
+    [ct.POINTER(ct.c_int), ct.POINTER(ct.c_int)]
+wrf_h.get_grid_size.restype = ct.c_int
+def get_grid_size(grid):
+    grid_num = ct.c_int(grid)
+    grid_size = ct.c_int()
+    wrf_h.get_grid_size(ct.byref(grid_num), ct.byref(grid_size))
+    return grid_size.value
+
+# Get the data type of the given variable as a string.
+wrf_h.get_var_type.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_VAR_NAME),
+     ct.POINTER(ct.c_char * bmi.BMI_MAX_TYPE_NAME)]
+wrf_h.get_var_type.restype = ct.c_int
+def get_var_type(name):
+    var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    var_name.value = name.encode()
+    var_type = ct.create_string_buffer(bmi.BMI_MAX_TYPE_NAME)
+    wrf_h.get_var_type(ct.byref(var_name), ct.byref(var_type))
+    return var_type.value.decode()
+
+# Get the units of the given variable.
+wrf_h.get_var_units.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_VAR_NAME),
+     ct.POINTER(ct.c_char * bmi.BMI_MAX_UNITS_NAME)]
+wrf_h.get_var_units.restype = ct.c_int
+def get_var_units(name):
+    var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    var_name.value = name.encode()
+    var_units = ct.create_string_buffer(bmi.BMI_MAX_UNITS_NAME)
+    wrf_h.get_var_units(ct.byref(var_name), ct.byref(var_units))
+    return var_units.value.decode()
+
+# Get memory use per array element, in bytes.
+wrf_h.get_var_itemsize.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_VAR_NAME),
+     ct.POINTER(ct.c_int)]
+wrf_h.get_var_itemsize.restype = ct.c_int
+def get_var_itemsize(name):
+    var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    var_name.value = name.encode()
+    var_size = ct.c_int()
+    wrf_h.get_var_itemsize(ct.byref(var_name), ct.byref(var_size))
+    return var_size.value
+
+# Get size of the given variable, in bytes.
+wrf_h.get_var_nbytes.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_VAR_NAME),
+     ct.POINTER(ct.c_int)]
+wrf_h.get_var_nbytes.restype = ct.c_int
+def get_var_nbytes(name):
+    var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    var_name.value = name.encode()
+    nbytes = ct.c_int()
+    wrf_h.get_var_nbytes(ct.byref(var_name), ct.byref(nbytes))
+    return nbytes.value
+
+# Time units of the model.
+wrf_h.get_time_units.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_UNITS_NAME)]
+wrf_h.get_time_units.restype = ct.c_int
+def get_time_units():
+    var_units = ct.create_string_buffer(bmi.BMI_MAX_UNITS_NAME)
+    wrf_h.get_time_units(ct.byref(var_units))
+    return var_units.value.decode()
+
+
 # -------------------------------
 # --- Working On Implementing ---
 # -------------------------------
+
 
 # Perform teardown tasks for the model.
 wrf_h.finalize.argtypes = []
@@ -86,54 +167,46 @@ def finalize():
     res = wrf_h.finalize()
     print("JK, ITS WORKING")
 
-
-# List a model's input variables.
-def get_input_var_names():
-    input_item_count = get_input_item_count()
-    print("iic is", input_item_count)
-    wrf_h.get_input_var_names.argtypes = \
-        [(ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME *
-                     input_item_count))]
-    wrf_h.get_input_var_names.restype = ct.c_int
-
-    names = (ct.c_char * bmi.BMI_MAX_COMPONENT_NAME *
-                     input_item_count)()
-    wrf_h.get_input_var_names(ct.byref(names))
-    print("---Python Todo---")
-    # names are returned from the function in a tuple, a standard container in
-    # the language.
-
-# Get the grid identifier for the given variable.
-wrf_h.get_var_grid.argtypes = \
-    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
-     ct.POINTER(ct.c_int)]
-wrf_h.get_var_grid.restype = ct.c_int
-def get_var_grid(name):
-    var_name = (ct.c_char * bmi.BMI_MAX_COMPONENT_NAME)()
-    var_name.value = name.encode('utf-8')
-    grid_num = ct.c_int()
-    wrf_h.get_var_grid(ct.byref(var_name), ct.byref(grid_num))
-    return grid_num.value
-
-# Get the total number of elements in the computational grid.
-wrf_h.get_grid_size.argtypes = \
-    [ct.POINTER(ct.c_int), ct.POINTER(ct.c_int)]
-wrf_h.get_grid_size.restype = ct.c_int
-
-
 # Get a copy of values (flattened!) of the given integer variable.
 wrf_h.get_value_int.argtypes = \
     [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
      ct.POINTER(ct.c_int)]
 wrf_h.get_value_int.restype = ct.c_int
-def get_value(var_name):
-    grid_num = ct.c_int()
-    grid_size = ct.c_int()
-    grid_num.value = 1
-    wrf_h.get_grid_size(grid_num, grid_size)
-    print(" IN PYTHON GET VALUE OF", var_name.value, "num",
-          grid_num.value, "size", grid_size.value)
+def get_value(name, array):
+    var_name = (ct.c_char * bmi.BMI_MAX_COMPONENT_NAME)()
+    var_name.value = name.encode()
+
+
 wrf_h.get_value = get_value
+
+
+# List a model's input variables.
+wrf_h.get_input_var_names.restype = ct.c_int
+def get_input_var_names():
+    # input items is input_item_count long, have to define func argtype after
+    # obtaining that number
+    input_item_count = get_input_item_count()
+    wrf_h.get_input_var_names.argtypes = \
+        [(ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME *
+                     input_item_count))]
+    names = (ct.c_char * bmi.BMI_MAX_COMPONENT_NAME *
+                     input_item_count)()
+    wrf_h.get_input_var_names(ct.byref(names))
+    print("---Python Todo---")
+    # print(names[1:3])
+    # print(''.join(names).split(b"\0"))
+    # print(''.join(str(names)).split(b"\0"))
+    # regular_array = bytearray.from_buffer(c_char_array)
+    # print(names)
+    # names are returned from the function in a tuple, a standard container in
+    # the language.
+    a = ['foobar' for i in range(input_item_count)]
+    return a
+
+# ------
+
+
+
 
 
 # Template for BMI Python C function definitions
