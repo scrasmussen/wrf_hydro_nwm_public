e8a4398e82c0833bb802bc91dc45673d79b60d32
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-05-14 23:42:28 -0600

    bmi: adding framework for python set and get var funcs

--- src/CPL/BMI_cpl/bmi_wrf_hydro_nwm/__init__.py
+++ src/CPL/BMI_cpl/bmi_wrf_hydro_nwm/__init__.py
@@ -122,7 +122,16 @@ def get_var_nbytes(name):
     wrf_h.get_var_nbytes(ct.byref(var_name), ct.byref(nbytes))
     return nbytes.value
 
-# todo: get_var_location
+# todo: needs to implemented in Fortran
+# Describe where a variable is located: node, edge, or face.
+# wrf_h.get_var_location.argtypes = [ct.POINTER(ct.)]
+# wrf_h.get_var_location.restype = ct.c_int
+# def get_var_location(name):
+#     var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+#     var_name.value = name.encode()
+#     location = ct.create_string_buffer(bmi.BMI_MAX_TYPE_NAME)
+#     wrf_h.get_var_location(ct.byref(name), ct.byref(location))
+#     return location.value.decode()
 
 # Current time of the model.
 wrf_h.get_current_time.argtypes = [ct.POINTER(ct.c_double)]
@@ -166,24 +175,225 @@ def get_time_step():
     wrf_h.get_time_step(ct.byref(var_units))
     return var_units.value.decode()
 
+def get_value(name, array):
+    var_type = get_var_type(name)
+    print("---", var_type)
+    if (var_type == 'integer'):
+        array = get_value_int(name, array)
+    elif (var_type == 'float'):
+        array = get_value_float(name, array)
+    elif (var_type == 'double'):
+        array = get_value_double(name, array)
+    return array
+
 # todo: get_value_int
+# Get a copy of values (flattened!) of the given integer variable.
+wrf_h.get_value_int.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_int)]
+wrf_h.get_value_int.restype = ct.c_int
+def get_value_int(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.get_value_int(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: get_value_float
+# Get a copy of values (flattened!) of the given float variable.
+wrf_h.get_value_float.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_float)]
+wrf_h.get_value_float.restype = ct.c_int
+def get_value_float(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.get_value_float(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: get_value_double
+# Get a copy of values (flattened!) of the given double variable.
+wrf_h.get_value_double.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_double)]
+wrf_h.get_value_double.restype = ct.c_int
+def get_value_double(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.get_value_double(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: get_value_ptr_int
 # todo: get_value_ptr_float
 # todo: get_value_ptr_double
+
+def get_value_at_indices(name, array):
+    var_type = get_var_type(name)
+    print("---", var_type)
+    if (var_type == 'integer'):
+        array = get_value_at_indices_int(name, array)
+    elif (var_type == 'float'):
+        array = get_value_at_indices_float(name, array)
+    elif (var_type == 'double'):
+        array = get_value_at_indices_double(name, array)
+    return array
+
 # todo: get_value_at_indices_int
+# Get a copy of values (flattened!) of the given integer variable.
+wrf_h.get_value_at_indices_int.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_int)]
+wrf_h.get_value_at_indices_int.restype = ct.c_int
+def get_value_at_indices_int(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.get_value_at_indices_int(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: get_value_at_indices_float
+# Get a copy of values (flattened!) of the given float variable.
+wrf_h.get_value_at_indices_float.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_float)]
+wrf_h.get_value_at_indices_float.restype = ct.c_int
+def get_value_at_indices_float(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.get_value_at_indices_float(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: get_value_at_indices_double
+# Get a copy of values (flattened!) of the given double variable.
+wrf_h.get_value_at_indices_double.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_double)]
+wrf_h.get_value_at_indices_double.restype = ct.c_int
+def get_value_at_indices_double(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.get_value_at_indices_double(ct.byref(var_name), ct.byref(array))
+    return array
+
+def set_value(name, array):
+    var_type = get_var_type(name)
+    print("---", var_type)
+    if (var_type == 'integer'):
+        array = set_value_int(name, array)
+    elif (var_type == 'float'):
+        array = set_value_float(name, array)
+    elif (var_type == 'double'):
+        array = set_value_double(name, array)
+    return array
+
 # todo: set_value_int
+# Get a copy of values (flattened!) of the given integer variable.
+wrf_h.set_value_int.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_int)]
+wrf_h.set_value_int.restype = ct.c_int
+def set_value_int(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.set_value_int(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: set_value_float
+# Get a copy of values (flattened!) of the given float variable.
+wrf_h.set_value_float.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_float)]
+wrf_h.set_value_float.restype = ct.c_int
+def set_value_float(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.set_value_float(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: set_value_double
+# Get a copy of values (flattened!) of the given double variable.
+wrf_h.set_value_double.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_double)]
+wrf_h.set_value_double.restype = ct.c_int
+def set_value_double(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.set_value_double(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: set_value_ptr_int
 # todo: set_value_ptr_float
 # todo: set_value_ptr_double
+
+def set_value_at_indices(name, array):
+    var_type = get_var_type(name)
+    print("---", var_type)
+    if (var_type == 'integer'):
+        array = set_value_at_indices_int(name, array)
+    elif (var_type == 'float'):
+        array = set_value_at_indices_float(name, array)
+    elif (var_type == 'double'):
+        array = set_value_at_indices_double(name, array)
+    return array
+
 # todo: set_value_at_indices_int
+# Get a copy of values (flattened!) of the given integer variable.
+wrf_h.set_value_at_indices_int.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_int)]
+wrf_h.set_value_at_indices_int.restype = ct.c_int
+def set_value_at_indices_int(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.set_value_at_indices_int(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: set_value_at_indices_float
+# Get a copy of values (flattened!) of the given float variable.
+wrf_h.set_value_at_indices_float.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_float)]
+wrf_h.set_value_at_indices_float.restype = ct.c_int
+def set_value_at_indices_float(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.set_value_at_indices_float(ct.byref(var_name), ct.byref(array))
+    return array
+
 # todo: set_value_at_indices_double
+# Get a copy of values (flattened!) of the given double variable.
+wrf_h.set_value_at_indices_double.argtypes = \
+    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
+     ct.POINTER(ct.c_double)]
+wrf_h.set_value_at_indices_double.restype = ct.c_int
+def set_value_at_indices_double(name, array):
+    # var_name = ct.create_string_buffer(bmi.BMI_MAX_VAR_NAME)
+    # var_name.value = name.encode()
+    # get_grid_size(name, grid_size)
+    # array = ct.
+    # wrf_h.set_value_at_indices_double(ct.byref(var_name), ct.byref(array))
+    return array
 
 # Get number of dimensions of the computational grid.
 wrf_h.get_grid_rank.argtypes = \
@@ -243,19 +453,6 @@ def finalize():
     res = wrf_h.finalize()
     print("JK, ITS WORKING")
 
-# Get a copy of values (flattened!) of the given integer variable.
-wrf_h.get_value_int.argtypes = \
-    [ct.POINTER(ct.c_char * bmi.BMI_MAX_COMPONENT_NAME),
-     ct.POINTER(ct.c_int)]
-wrf_h.get_value_int.restype = ct.c_int
-def get_value(name, array):
-    var_name = (ct.c_char * bmi.BMI_MAX_COMPONENT_NAME)()
-    var_name.value = name.encode()
-
-
-wrf_h.get_value = get_value
-
-
 # list a model's input variables.
 wrf_h.get_input_var_names.restype = ct.c_int
 def get_input_var_names():
@@ -280,7 +477,6 @@ def get_input_var_names():
     return a
 
 # ------
-
 
 
 # Template for BMI Python C function definitions
