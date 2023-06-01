80bb1a5b2cf71d56091e5ff118d52eb48fc90f46
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-06-01 13:54:56 -0600

    bmi: wrf-hydro modflow hello world update

--- src/CPL/MODFLOW_cpl/wrf_hydro_nwm_modflow6_hello_world.f90
+++ src/CPL/MODFLOW_cpl/wrf_hydro_nwm_modflow6_hello_world.f90
@@ -1,122 +1,65 @@
 program wrf_hydro_nwm_bmi_driver
-  use bmi_wrf_hydro_nwm_mod, only: bmi_wrf_hydro_nwm, stat_check!, BMI_SUCCESS
+  use bmi_wrf_hydro_nwm_mod, only: bmi_wrf_hydro_nwm, stat_check, BMI_SUCCESS
   use bmi_wrf_hydro_nwm_mod, only: BMI_MAX_COMPONENT_NAME, BMI_MAX_VAR_NAME
   use bmi_wrf_hydro_nwm_mod, only: BMI_MAX_TYPE_NAME, BMI_MAX_UNITS_NAME
   use bmi_wrf_hydro_nwm_mod, only: wrf_hydro_nwm
-  use mf6bmi
-  ! use bmi_modflow_mod, only: modflow6, bmi_modflow
+  use bmi_modflow_mod, only : modflow6, bmi_modflow, BMI_LENCOMPONENTNAME
+  use iso_c_binding, only : c_char, C_NULL_CHAR
   implicit none
-  ! type(bmi_modflow) :: modflow
 
   type(bmi_wrf_hydro_nwm) :: wrf_hydro
+  type(bmi_modflow) :: modflow
+
   character(len=BMI_MAX_COMPONENT_NAME), pointer :: model_name
+  character(len=256), pointer :: mf_model_name
+
   character(len=BMI_MAX_VAR_NAME) :: time_unit
-  character(c_char) :: mf_model_name(256) ! BMI_LENCOMPONENTNAME
-  double precision :: end_time, current_time, time_step
+  character(len=BMI_MAX_VAR_NAME) :: mf_time_unit
+  double precision :: end_time, current_time, time_step, mf_time_step
   integer :: res, stat, i
   integer, allocatable :: end(:), var_data(:), grid_shape(:)
   integer, allocatable :: ind_data(:), indices_data(:), indices(:)
   integer, pointer ::  ptr_data(:)
+  real, allocatable :: foo_data(:)
   stat = BMI_SUCCESS
 
   wrf_hydro = wrf_hydro_nwm()
-  ! modflow = modflow6()
+  modflow = modflow6()
   call stat_check(wrf_hydro%get_component_name(model_name), stat)
-  call stat_check(bmi_get_component_name(mf_model_name), stat)
-  end = findloc(mf_model_name, C_NULL_CHAR)
+  call stat_check(modflow%get_component_name(mf_model_name), stat)
 
-  print * , "--- Starting ", trim(model_name), " and ", &
-       mf_model_name(1:end(1)), " ---"
+  print * , "--- Starting ", trim(model_name), " and ", trim(mf_model_name), " ---"
 
   call stat_check(wrf_hydro%initialize("no config file"), stat)
-  call stat_check(bmi_initialize(), stat)
+  call stat_check(modflow%initialize(""), stat)
+
+  call stat_check(wrf_hydro%get_start_time(current_time), stat)
+  call stat_check(wrf_hydro%get_end_time(end_time), stat)
+
+  call stat_check(wrf_hydro%get_time_step(time_step), stat)
+  call stat_check(modflow%get_time_step(mf_time_step), stat)
+
+  call stat_check(wrf_hydro%get_time_units(time_unit), stat)
+  call stat_check(modflow%get_time_units(mf_time_unit), stat)
+  print *, "end time:", end_time, "current time:", current_time
+
+  do while (current_time < end_time)
+     ! update models
+     call stat_check(wrf_hydro%update(), stat)
+     if (mod(current_time, mf_time_step) == 0) then
+        call stat_check(modflow%update(), stat)
+     end if
+
+     ! transfer variables
+     call stat_check(modflow%get_value("foo", foo_data), stat)
+     call stat_check(wrf_hydro%set_value("soldrain", foo_data), stat)
+
+     call stat_check(wrf_hydro%get_current_time(current_time), stat)
+  end do
 
 
   call stat_check(wrf_hydro%finalize(), stat)
-  call stat_check(bmi_finalize(), stat)
+  ! call stat_check(modflow%finalize(), stat)
   print *, "--- FIN ---"
 
-
-
-
-
-
-
-
-  ! ! call stat_check(wrf_hydro%get_start_time(current_time), stat)
-  ! ! call stat_check(wrf_hydro%get_end_time(end_time), stat)
-
-  ! call stat_check(wrf_hydro%get_time_units(time_unit), stat)
-  ! print *, "time unit is: ", trim(time_unit)
-
-  ! call stat_check(wrf_hydro%get_input_item_count(input_item_count), stat)
-  ! call stat_check(wrf_hydro%get_input_var_names(input_var_names), stat)
-  ! do i=1,input_item_count
-  !    print *, "input item", i, ":", trim(input_var_names(i))
-  ! end do
-
-  ! call stat_check(wrf_hydro%get_output_item_count(output_item_count), stat)
-  ! call stat_check(wrf_hydro%get_output_var_names(output_var_names), stat)
-  ! do i=1,output_item_count
-  !    print *, "output item", i, ":", trim(output_var_names(i))
-  ! end do
-
-  ! call stat_check(wrf_hydro%get_var_grid("IVGTYP", var_grid), stat)
-  ! ! res = wrf_hydro%get_var_grid("ISLTYP", var_grid)
-
-  ! call stat_check(wrf_hydro%get_var_type(trim(output_var_names(var_grid)), type_name), stat)
-  ! call stat_check(wrf_hydro%get_var_units(trim(output_var_names(var_grid)), var_unit), stat)
-  ! call stat_check(wrf_hydro%get_grid_size(var_grid, grid_size), stat)
-  ! call stat_check(wrf_hydro%get_var_itemsize(trim(output_var_names(var_grid)), item_size), stat)
-  ! call stat_check(wrf_hydro%get_var_nbytes(trim(output_var_names(var_grid)), nbytes), stat)
-
-  ! print *, "var ", trim(output_var_names(var_grid)), " is of type ", trim(type_name), &
-  !      " and has units ", trim(var_unit)
-  ! print *, "var ", trim(output_var_names(var_grid)), " is grid", var_grid, &
-  !      " also has grid size", grid_size, "has item size", item_size, "so naturally nbytes of", nbytes
-
-
-  ! allocate(var_data(grid_size))
-  ! call stat_check(wrf_hydro%get_value(trim(output_var_names(var_grid)), var_data), stat)
-  ! print *, "getting data", var_data(228:230), "should match 11, 5, 11"
-  ! print *, "-----", stat
-  ! call stat_check(wrf_hydro%get_grid_shape(var_grid, grid_shape), stat)
-  ! print *, "Shape is ", grid_shape, " and stat" ,stat
-
-  ! print *, "====== PTR TEST ======"
-  ! call stat_check(wrf_hydro%get_value_ptr(trim(output_var_names(var_grid)), ptr_data), stat)
-  ! stat = BMI_SUCCESS
-
-  ! print *, "ptr_data associated value is ", associated(ptr_data)
-
-  ! print *, "====== GET AT INDICES ======"
-
-  ! ! ind_data: need to allocate?
-  ! indices = [228,230,229]
-  ! allocate(ind_data(size(indices)))
-  ! call stat_check(wrf_hydro%get_value_at_indices(trim(output_var_names(var_grid)), ind_data, &
-  !      [228,230,229]), stat)
-
-  ! print *, "got the data from indices:", ind_data
-
-  ! print *, "====== SETTING DATA ======"
-  ! var_data = -77
-  ! call stat_check(wrf_hydro%set_value(trim(output_var_names(var_grid)), var_data), stat)
-  ! print *, "-- now by indices"
-  ! indices = [2,4,20]
-  ! indices_data = [11., 12., 13.]
-  ! call stat_check(wrf_hydro%set_value_at_indices(trim(output_var_names(var_grid)), &
-  !      indices, indices_data), stat)
-
-
-  ! do while (current_time < end_time)
-  !    call stat_check(wrf_hydro%update(), stat)
-  !    call stat_check(wrf_hydro%get_current_time(current_time), stat)
-  ! end do
-
-  ! print *, "--- Finished running test driver ---"
-  ! if (stat .ne. BMI_SUCCESS) then
-  !    print *, "WRF-Hydro driver returned failure"
-  !    error stop stat
-  ! end if
 end program wrf_hydro_nwm_bmi_driver
