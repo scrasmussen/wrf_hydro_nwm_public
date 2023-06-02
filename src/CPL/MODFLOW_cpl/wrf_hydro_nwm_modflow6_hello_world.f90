64a8a498d401b406b967f5728aa42dbc03c5ec3c
Soren Rasmussen <s.c.rasmussen@gmail.com>
2023-06-02 09:40:59 -0600

    bmi: added roughly how soldrain will be handled into modflow hello world example

--- src/CPL/MODFLOW_cpl/wrf_hydro_nwm_modflow6_hello_world.f90
+++ src/CPL/MODFLOW_cpl/wrf_hydro_nwm_modflow6_hello_world.f90
@@ -5,6 +5,7 @@ program wrf_hydro_nwm_bmi_driver
   use bmi_wrf_hydro_nwm_mod, only: wrf_hydro_nwm
   use bmi_modflow_mod, only : modflow6, bmi_modflow, BMI_LENCOMPONENTNAME
   use iso_c_binding, only : c_char, C_NULL_CHAR
+  use mf6bmiUtil, only:  BMI_LENVARADDRESS
   implicit none
 
   type(bmi_wrf_hydro_nwm) :: wrf_hydro
@@ -22,6 +23,14 @@ program wrf_hydro_nwm_bmi_driver
   integer, pointer ::  ptr_data(:)
   real, allocatable :: foo_data(:)
   integer :: mf_input_count, mf_output_count
+  character(len=BMI_MAX_VAR_NAME), pointer :: names(:)
+
+  ! soldrain
+  integer :: soldrain_grid, soldrain_rank, soldrain_size
+  integer, allocatable :: soldrain_grid_shape(:)
+  integer :: soldrain_grid_shape_const(2)
+  real, allocatable :: soldrain(:,:), soldrain_flat(:)
+
 
   stat = BMI_SUCCESS
 
@@ -41,28 +50,40 @@ program wrf_hydro_nwm_bmi_driver
   call stat_check(wrf_hydro%get_end_time(end_time), stat)
   call stat_check(wrf_hydro%get_time_step(time_step), stat)
   call stat_check(modflow%get_time_step(mf_time_step), stat)
-  print *, "TIME STEPS:", time_step, mf_time_step
   call stat_check(wrf_hydro%get_time_units(time_unit), stat)
-  call stat_check(modflow%get_time_units(mf_time_unit), stat)
-  print *, "end time:", end_time, "current time:", current_time
+  ! call stat_check(modflow%get_time_units(mf_time_unit), stat) ! hardcoded, need to update
 
-  call stat_check(modflow%get_input_item_count(mf_input_count), stat)
-  call stat_check(modflow%get_output_item_count(mf_output_count), stat)
-  print *, "MODFLOW: input count", mf_input_count, "output count", mf_output_count
+  ! --- setup soldrain variables
+  call stat_check(wrf_hydro%get_var_grid("soldrain", soldrain_grid), stat)
+  call stat_check(wrf_hydro%get_grid_rank(soldrain_grid, soldrain_rank), stat)
+  call stat_check(wrf_hydro%get_grid_shape(soldrain_grid, soldrain_grid_shape), stat)
+  soldrain_grid_shape_const = soldrain_grid_shape
+  call stat_check(wrf_hydro%get_grid_size(soldrain_grid, soldrain_size), stat)
+  allocate(soldrain_flat(soldrain_size))
+  allocate(soldrain(soldrain_grid_shape(1), soldrain_grid_shape(2)))
+  ! --- done setting up soldrain variables
 
 
   do while (current_time < end_time)
      ! update models
      call stat_check(wrf_hydro%update(), stat)
      call stat_check(modflow%update(), stat)
+     ! the math for when to update modflow needs to be checked
      ! if (mod(current_time, mf_time_step) == 0) then
      !    call stat_check(modflow%update(), stat)
      ! end if
 
-     ! transfer variables
+     ! --- transfer variables
+     ! get current values
      ! call stat_check(modflow%get_value("foo", foo_data), stat)
-     ! call stat_check(wrf_hydro%set_value("soldrain", foo_data), stat)
+     call stat_check(wrf_hydro%get_value("soldrain", soldrain_flat), stat)
+     soldrain = reshape(soldrain_flat, soldrain_grid_shape_const)
 
+     ! update soldrain value
+     ! soldrain = soldrain + update_value
+     call stat_check(wrf_hydro%set_value("soldrain", pack(soldrain, .true.)), stat)
+
+     ! update current_time
      call stat_check(wrf_hydro%get_current_time(current_time), stat)
   end do
 
